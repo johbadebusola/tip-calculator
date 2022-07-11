87ff74b04d8fd4871aa2711d2c22f30faa3e8db4
Oluwatoyin John Adebusola <101136827+johbadebusola@users.noreply.github.com>
2022-07-11 13:36:55 +0100

    Update js
    Code for looping through the tip button

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 
+let bill = document.querySelector("#bill");
+let numOfPeople = document.querySelector("#people");
+let tipBtn = document.querySelectorAll("#btn");
+let tipAmountOutput = document.querySelector("#p2");
+let totalBillPerPersonOutput = document.querySelector("#p3");
+
+// looping through each of the tip button
+tipBtn.forEach(e =>{
+    e.addEventListener("click", calculate)
+})
