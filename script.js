f3465b74dff119e12c817ffc601b07fe56af7734
Oluwatoyin John Adebusola <101136827+johbadebusola@users.noreply.github.com>
2022-07-12 21:24:07 +0100

    Update script.js
    Added the calculations for the custom button

--- script.js
+++ script.js
@@ -41,14 +41,49 @@ reset.setAttribute("style","background-color:hsl(172, 67%, 45%); color:hsl(183, 
 }
 
 //Reset button
-let reset = document.querySelector("#reset");
-reset.addEventListener("click", e =>{
+let reset =  document.querySelector("#reset");
+
+reset.addEventListener("click", resetBtn)
+
+function resetBtn(){
     totalBillPerPersonOutput.innerHTML = "$0.00";
     tipAmountOutput.innerHTML = "$0.00";
-    bill.value = "";
-    numOfPeople.value = "";
-    reset.setAttribute("style","background-color:hsl(183, 100%, 10%);")
-})
+bill.value = "";
+numOfPeople.value = "";
+custom.value ="";
+   reset.setAttribute("style","background-color:hsl(183,100%,10%);")
+}
+
+// Caculations for custom
+let custom = document.querySelector(".custom");
+
+custom.addEventListener("input",customCalc)
+
+function customCalc(){
+
+// To validate if number of people is entered
+if (numOfPeople.value.trim() === "") {
+    numOfPeople.setAttribute("style","border: 2px solid tomato");
+    errorMessage.style.opacity = "1";
+    return;
+}else{
+    numOfPeople.setAttribute("style","border: none")
+    errorMessage.style.opacity = "0";
+}
+
+// Calculations for amount of tip per person
+let tipAmountPerPerson = ((this.value/100) * bill.value) / numOfPeople.value;
+tipAmountOutput.innerHTML = "$" + tipAmountPerPerson.toFixed(2);
+
+// Calculations for amount of total amount  per person
+let billPerPerson = bill.value / numOfPeople.value;
+let totalBillPerPerson = +billPerPerson + +tipAmountPerPerson;
+totalBillPerPersonOutput.innerHTML = "$" +  totalBillPerPerson.toFixed(2);
+console.log(totalBillPerPerson.toFixed(2));
+
+reset.setAttribute("style","background-color:hsl(172, 67%, 45%); color:hsl(183, 100%, 15%);")
+
+}
 
 
 
